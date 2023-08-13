19df3328a3570a1b308f4d22f7865e3c869486b6
kvp <abc@def.con>
2023-08-13 10:59:44 +0200

    Create test1.ino

---
+++ test1/test1.ino
@@ -0,0 +1,43 @@
+#define LED_FROM (24)
+#define LED_TO   (51)
+
+#define KEY_FROM (A0)
+#define KEY_TO   (A15)
+
+#define BOARD_LED (13)
+
+int ledNum = LED_FROM;
+
+void setup()
+{
+  Serial.begin(115200);
+
+  pinMode(BOARD_LED, OUTPUT);
+  digitalWrite(BOARD_LED, LOW);
+
+  for (int t = LED_FROM; t <= LED_TO; t++)
+  {
+    pinMode(t, OUTPUT);
+    digitalWrite(t, LOW);
+  }
+  for (int t = KEY_FROM; t <= KEY_TO; t++)
+    pinMode(t, INPUT_PULLUP);
+
+  Serial.println();
+  Serial.println();
+  Serial.println("Domino 55 - test1");
+}
+
+void loop()
+{
+  digitalWrite(ledNum, HIGH);
+  delay(1000);
+  digitalWrite(ledNum, LOW);
+  ledNum++;
+  if (ledNum > LED_TO)
+    ledNum = LED_FROM;
+  for (int t = KEY_FROM; t <= KEY_TO; t++)
+    Serial.print(digitalRead(t), BIN);
+  Serial.println();
+}
+
