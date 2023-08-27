073917eb7d6d341ab557717f319201c71b91c60e
kvp <abc@def.con>
2023-08-27 03:09:59 +0200

    Update d55.ino

--- d55/d55.ino
+++ d55/d55.ino
@@ -2,11 +2,17 @@
 
 #define LED_FROM (24)
 #define LED_TO   (51)
+#define LED_NUM  (28)
 
 #define KEY_FROM (A0)
 #define KEY_TO   (A15)
+#define KEY_NUM  (16) // only using the 1st 15
 
 #define BOARD_LED (13)
+
+int keys[KEY_NUM] = {};
+int leds[LED_NUM] = {1,1,1,2,2,2,3,3,3};
+bool blinkStep = 0;
 
 void setup()
 {
@@ -31,6 +37,12 @@ void setup()
 
 void loop()
 {
+  for (int t = 0; t < KEY_NUM; t++)
+    keys[t] = digitalRead(KEY_FROM + t);
 
+  blinkStep = ((millis() / 500) & 1);
+  int ledsMask = ((blinkStep == 0) ? 1 : 2);
+  for (int t = 0; t < LED_NUM; t++)
+    digitalWrite(LED_FROM + t, (((leds[t] & ledsMask) != 0) ? HIGH : LOW));
 }
 
